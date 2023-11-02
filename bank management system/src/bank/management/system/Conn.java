a59b6e824ce05976a053a059f595f08808da4e34
Swarupa Pachpande <109409691+swarupapachpande@users.noreply.github.com>
2023-11-02 22:07:08 +0530

    Add files via upload

---
+++ bank management system/src/bank/management/system/Conn.java
@@ -0,0 +1,18 @@
+package bank.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+         //  Class.forName(com.mysql.cj.jdbc.Driver);
+           c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "root");
+           s = c.createStatement();
+        }catch(Exception e){
+            System.out.println(e);
+        } 
+    }
+}
