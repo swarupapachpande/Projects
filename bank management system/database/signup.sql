ac43c906f52a84b745725d4c66b6ee609d1ff2bd
Swarupa Pachpande <109409691+swarupapachpande@users.noreply.github.com>
2023-11-02 22:07:40 +0530

    Add files via upload

---
+++ bank management system/database/signup.sql
@@ -0,0 +1,10 @@
+create database bankmanagementsystem;
+use bankmanagementsystem;
+show databases;
+
+create table signup(formno varchar(20), name varchar(20), father_name varchar(20),dob varchar(20),gender varchar(20),email varchar(30),marital_status varchar(20),address varchar(50),city varchar(25),pincode varchar(20),state varchar(25));
+
+show tables;
+
+select * from signup;
+
